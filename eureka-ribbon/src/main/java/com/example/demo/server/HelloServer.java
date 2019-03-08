5f857349273a0f2518df08945586b3b910dae8a9
zzy <120157229@qq.com>
2019-03-08 10:07:33 +0800

    在ribbon使用断路器

--- eureka-ribbon/src/main/java/com/example/demo/server/HelloServer.java
+++ eureka-ribbon/src/main/java/com/example/demo/server/HelloServer.java
@@ -1,5 +1,6 @@
 package com.example.demo.server;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -10,7 +11,12 @@ public class HelloServer {
     @Autowired
     RestTemplate restTemplate;
 
+    @HystrixCommand(fallbackMethod = "hiError")
     public String hiService(String name) {
         return restTemplate.getForObject("http://SERVICE-HI/hi?name=" + name, String.class);
+    }
+
+    public String hiError(String name) {
+        return "hi," + name + ",sorry,error!";
     }
 }
