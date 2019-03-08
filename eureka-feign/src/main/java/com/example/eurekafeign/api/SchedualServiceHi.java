a351a63f452db23e56880662e50ab73cb61df299
zzy <120157229@qq.com>
2019-03-08 09:47:06 +0800

    提交服务消费者（Feign），格式化代码

--- eureka-feign/src/main/java/com/example/eurekafeign/api/SchedualServiceHi.java
+++ eureka-feign/src/main/java/com/example/eurekafeign/api/SchedualServiceHi.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(value = "service-hi")
 public interface SchedualServiceHi {
+
     @RequestMapping(value = "/hi", method = RequestMethod.GET)
     String sayHiFromClientOne(@RequestParam(value = "name") String name);
+
 }
--- eureka-feign/src/main/java/com/example/eurekafeign/web/HiController.java
+++ eureka-feign/src/main/java/com/example/eurekafeign/web/HiController.java
@@ -12,7 +12,7 @@ public class HiController {
 
     //编译器报错，无视。 因为这个Bean是在程序启动的时候注入的，编译器感知不到，所以报错。
     @Autowired
-    SchedualServiceHi schedualServiceHi;
+    private SchedualServiceHi schedualServiceHi;
 
     @GetMapping(value = "/hi")
     public String sayHi(@RequestParam String name) {
