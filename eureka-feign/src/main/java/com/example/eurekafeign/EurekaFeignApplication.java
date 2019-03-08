fd46ba8303deb93a6a2c38718c99f2cc48fe121d
zzy <120157229@qq.com>
2019-03-08 09:44:22 +0800

    提交服务消费者（Feign）

---
+++ eureka-feign/src/main/java/com/example/eurekafeign/EurekaFeignApplication.java
@@ -0,0 +1,19 @@
+package com.example.eurekafeign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
+@EnableFeignClients
+public class EurekaFeignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaFeignApplication.class, args);
+    }
+
+}
