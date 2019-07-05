507141ee227201765b377cef7a17b0855f978720
zhangzhenyan <120157229@qq.com>
2019-07-05 16:28:39 +0800

    更新代码

--- eureka-feign/src/main/java/com/example/eurekafeign/web/HiController.java
+++ eureka-feign/src/main/java/com/example/eurekafeign/web/HiController.java
@@ -1,15 +1,16 @@
 package com.example.eurekafeign.web;
 
 import com.example.eurekafeign.api.SchedualServiceHi;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+
 @RestController
 public class HiController {
 
-    @Autowired
+    @Resource
     private SchedualServiceHi schedualServiceHi;
 
     @GetMapping(value = "/hi")
